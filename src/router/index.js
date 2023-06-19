6fda9a7141fa4e08f91e9a0f9bceeeba6972541a
cloudpunchx <siobhanbonardi@hotmail.com>
2023-06-19 01:01:54 -0600

    Changed title. Ready to launch and add to portfolio.

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     component: HomeView,
     meta: [
       {
-        title: "Cute Calculator"
+        title: "Simple Calculator"
       },
       {
         name: "description",
