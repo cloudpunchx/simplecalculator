f40fd86e5089542bee36d0d7f982b0079681a5b8
cloudpunchx <siobhanbonardi@hotmail.com>
2023-06-19 00:45:46 -0600

    Simple calculator build, need to finish title styling with imported font.

--- src/router/index.js
+++ src/router/index.js
@@ -7,21 +7,56 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'HomeView',
+    component: HomeView,
+    meta: [
+      {
+        title: "Cute Calculator"
+      },
+      {
+        name: "description",
+        content: "Calculator"
+      },
+      {
+        name: "keywords",
+        content: "calculator, cute, math, functional"
+      },
+      {
+        name: "author",
+        content: "Siobhan Bonardi"
+      }
+    ]
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to,from,next)=>{
+  // Get all meta tags 
+  let metaTags = document.querySelectorAll('meta');
+  // Loop through the tags and remove each
+  // for OF is vanilla Javascript so that's what we have to use (vue is for in loops) and this page is PURE JAVASCRIPT
+  for (let tag of metaTags){
+    tag.remove();
+  }
+  // to.meta (navigating TO.meta.title)
+  let newTags = to.meta;
+  document.querySelector('title').innerText = newTags[0].title;
+
+  // i zero is the title obj that we don't need here
+  for (let i = 1; i<newTags.length; i++){
+    document.querySelector(`head`).insertAdjacentHTML(`afterbegin`,
+                                                      `<meta name:"${newTags[i].name}" description="${newTags[i].content}">`);
+  }
+  document.querySelector(`head`).insertAdjacentHTML(`afterbegin`,
+                                                    `<meta charset="utf-8">
+                                                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                                    <meta name="viewport" content="width=device-width,initial-scale=1.0">`
+                                                    );
+  next();
+  from;
 })
 
 export default router
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,135 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <p class="title">Simple Calculator</p>
+        <div class="calculator">
+            <div class="outputContainer">
+                <div class="outputCalc">{{ calculatorValue }}</div>
+            </div>
+            <div class="buttons">
+                <div
+                class="button"
+                v-for="n in btnArr"
+                :key="n"
+                :class="{ operator: ['C', '*', '/', '-', '+', '%', '='].includes(n) }"
+                >
+                    <div class="btn" @click="calculatorFunction(n)">
+                        {{ n }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: "HomeView",
+        data() {
+            return {
+                calculatorValue: "",
+                btnArr: [
+                    "C",
+                    "*",
+                    "/",
+                    "-",
+                    "7",
+                    "8",
+                    "9",
+                    "+",
+                    "4",
+                    "5",
+                    "6",
+                    "%",
+                    "1",
+                    "2",
+                    "3",
+                    "=",
+                    "0",
+                    ".",
+                ],
+            };
+        },
+        methods: {
+            // (n) is the value of the button clicked
+            calculatorFunction(n) {
+            /* Append value */
+            if (!isNaN(n) || n === ".") {
+                this.calculatorValue += n + "";
+            }
+            /* Clear value, reset string to empty */
+            if (n === "C") {
+                this.calculatorValue = "";
+            }
+            /* Percentage calculates by 100 */
+            if (n === "%") {
+                this.calculatorValue = this.calculatorValue / 100 + "";
+            }
+            /* Operators divide, multiply, subtract, add */
+            if (["/", "*", "-", "+"].includes(n)) {
+                this.operator = n;
+                this.previousCalculatorValue = this.calculatorValue;
+                this.calculatorValue = "";
+            }
+            /* Calculate result using the eval function */
+            if (n === "=") {
+                this.calculatorValue = eval(
+                this.previousCalculatorValue + this.operator + this.calculatorValue
+                );
+                this.previousCalculatorValue = "";
+                this.operator = null;
+            }
+        },
+    },
+};
 </script>
+
+<style scoped>
+.title{
+    text-align: center;
+    font-size: 22pt;
+}
+.calculator{
+    margin-top: 100px;
+}
+.outputContainer {
+    text-align: right;
+    background-color: #F97068;
+    width: min(300px, 70%);
+    margin: 10px auto;
+    padding: 10px;
+    border: 0.5px solid rgb(255, 198, 124);
+    border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+}
+.outputCalc {
+    background-color: #EDF2EF;
+    padding: 20px;
+    border-radius: 3px;
+}
+.buttons {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: minmax(50px, auto);
+    width: min(300px, 70%);
+    border: 0.2px solid rgb(255, 198, 124);
+    border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+    align-items: center;
+    background-color: #F97068;
+    justify-content: center;
+    margin: 10px auto;
+    gap: 5px;
+}
+.button {
+    background-color: #D1D646;
+    padding: 6px;
+    border-radius: 5px;
+    margin: 5px;
+    font-weight: bold;
+    font-size: 1.5em;
+    color: whitesmoke;
+    text-align: center;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+}
+</style>
